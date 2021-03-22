12d91732233210bb25c3756897200272666ab2ea
Angelina <angelina@Angelinas-iMac.local>
2021-03-22 16:54:55 +0000

    Updated some comments

--- secondstring.py
+++ secondstring.py
@@ -1,17 +1,19 @@
 #This program asks a user to input a string and outputs every second 
-# letter in reverse order.
+#letter in reverse order.
+#author: Angelina Belotserkovskaya
 
 inpStr = input('Please enter a sentence: ')
 
+# 1st solution:
 #saving every second letter from the input string into secLetter variable
 #secLetter = inpStr[1::2]
 
 #reversing the string, example can be found below:
 #  https://www.w3schools.com/python/python_howto_reverse_string.asp
 #reverStr = secLetter[::-1]
-
 #print (reverStr)
 
+# 2nd solution:
 #shorter version:
 secLetter = inpStr[1::2][::-1]
 print(secLetter)
