4b2861d195d16726815ab95e0240bdaecad7665c
Angelina <angelina@Angelinas-iMac.local>
2021-03-23 14:06:18 +0000

    Added while loop

--- bmi.py
+++ bmi.py
@@ -1,25 +1,24 @@
 #This program calculates Body Mass Index (BMI)
 #author: Angelina Belotserkovskaya
 
-
-try:
+while True:
+    try:
     # Ask to input height in cm and save it into variable 
     # which is converted into int
-    height = int(input('Please enter your height in centimetres: '))
-    # Convert cm to m2
-    heightInM2 = (height**2) / 10000
-
+        height = int(input('Please enter your height in centimetres: '))
+    
     # Ask to input weight in kg and save it into variable 
     # which is converted into int
+        weight = int(input('Please enter your weight in kg: '))
+        break
+    except ValueError:
+        print('Please make sure to enter an integer number')
 
-    weight = int(input('Please enter your weight in kg: '))
-
-    # Calculating bmi
-    bmi = weight/heightInM2
-    print ('Your BMI is {:.2f}'.format(bmi))
-except:
-    print('Please make sure to enter an integer number')
-
+# Convert cm to m2
+heightInM2 = (height**2) / 10000
+# Calculating bmi
+bmi = weight/heightInM2
+print ('Your BMI is {:.2f}'.format(bmi))
 
 
 
